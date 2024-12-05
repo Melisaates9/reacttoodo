e217e001e74c909233d914eeb22d420f458ac872
melisa ateş <melisaserap@gmail.com>
2024-12-05 20:01:22 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,57 @@
-import { useState } from "react"
-import styled from "styled-components"
-
+import { useState } from "react";
+import styled from "styled-components";
 
 function App() {
-  const [input,setInput]=useState('')
-  const [todos,setTodos]=useState([])
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
   const Wrapper = styled.div`
+    text-align: center;
+    height: 100vh;
+    padding-top: 300px;
+  `;
 
-  text-align:center;
-  height:100vh;
-  padding-top:300px;
-  
+  const Input = styled.input`
+    padding: 10px;
+  `;
+  const Button = styled.button`
+    padding: 10px;
+    background-color: #49416d;
+    color: white;
+    border: 2px solid;
+  `;
 
-    
-  `
+  function changeInput(e) {
+    setInput(e.target.value);
+  }
 
-  const Input= styled.input`
-  padding: 10px;
-
-  
-  `
-  const Button=styled.button`
-padding: 10px;
-background-color: #49416d;
- color: white
- ;
- border: 2px solid 
-
-  `
-
-
-  function changeInput(e){
-  setInput(e.target.value)
-}
-
-function addToDo (){
-setTodos([...todos,input])
-setInput('')
-}
-function deleteTodo(value) {
-  setTodos((todos)=>{
-    return todos.filter((todo)=>todo!==value);
-  })
-}
+  function addToDo() {
+    setTodos([...todos, input]);
+    setInput("");
+  }
+  function deleteTodo(value) {
+    setTodos((todos) => {
+      return todos.filter((todo) => todo !== value);
+    });
+  }
 
   return (
     <>
-    <Wrapper>
-
-   
-      <Input
-      onChange={changeInput}
-      value={input}
-type="text"
-autoFocus
-/>
-      <Button onClick={addToDo}>
-        Ekle
-      </Button>
-      <div>
-        <ul style={{cursor:'pointer'}}>
-          {
-            todos.map((todo,index)=>(
+      <Wrapper>
+        <Input onChange={changeInput} value={input} type="text" autoFocus />
+        <Button onClick={addToDo}>Ekle</Button>
+        <div>
+          <ul style={{ cursor: "pointer" }}>
+            {todos.map((todo, index) => (
               //index listeler içerisine bakıp hangi numaralı olanın gelceğini sıralıyor
-              <li key={index} onClick={()=>deleteTodo(todo)}>   
-              {todo}
+              <li key={index} onClick={() => deleteTodo(todo)}>
+                {todo}
               </li>
-            ))
-          }
-        </ul>
-      </div>
+            ))}
+          </ul>
+        </div>
       </Wrapper>
     </>
-  )
+  );
+}
 
-} 
-
-
-export default App
+export default App;
